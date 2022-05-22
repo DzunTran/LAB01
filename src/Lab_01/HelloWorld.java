4aa4e139f72f3061b55ee9e4418ef8f70e658bdf
Dzun Tran <dungtranx.4991@gmail.com>
2022-05-22 21:40:59 +0700

    new

--- src/Lab_01/HelloWorld.java
+++ src/Lab_01/HelloWorld.java
@@ -2,6 +2,6 @@ package Lab_01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!!...!!");
+        System.out.println("HelloWorld!!!..");
     }
 }
